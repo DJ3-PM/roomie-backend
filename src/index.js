4039ebf0e5695995b52495a949ba8648cb4625f6
Jose Cuevas <josecueram@gmail.com>
2020-06-26 12:57:27 -0500

    [Add] Stantard linter & rules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const { config } = require("../config/index");
+const { config } = require('../config/index')
 
 // app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.send("xd");
-});
+app.get('/', function (req, res) {
+  res.send('xd')
+})
 
 app.listen(config.port, function () {
-  console.log(`Listening http://localhost:${config.port}`);
-});
+  console.log(`Listening http://localhost:${config.port}`)
+})
