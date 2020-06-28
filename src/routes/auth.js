a15680b79927dfadf2fad0e5334c535802607ede
Jose Cuevas <josecueram@gmail.com>
2020-06-27 19:48:44 -0500

    [ADD] Basic Users Service functionality to create User

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const usersService = require('../services/users')
 
 const authRoutes = app => {
   const router = express.Router()
   app.use('/api/auth', router)
 
-  router.post('/sign-up', (req, res, next) => {
+  router.post('/sign-up', async (req, res, next) => {
     const user = req.body
+
+    try {
+      const createdUserId = await usersService.createUser({ user })
+      res.status(201).json({
+        data: createdUserId,
+        message: 'User created!'
+      })
+    } catch (error) {
+      next(error)
+    }
   })
 }
 
