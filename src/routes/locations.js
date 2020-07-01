379e815ff7f0e6dcea76654939e30edb93422677
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-01 17:10:35 -0500

    [Add] get neighborhoods in zone

--- src/routes/locations.js
+++ src/routes/locations.js
@@ -5,6 +5,7 @@ const locationsRoutes = app => {
   const router = express.Router()
   app.use('/api/locations', router)
 
+  // ? Lists all the zones
   router.get('/', async (req, res, next) => {
     try {
       const locations = await locationsServices.getLocations()
@@ -12,6 +13,20 @@ const locationsRoutes = app => {
       res.status(200).json({
         data: locations,
         message: 'Zones listed'
+      })
+    } catch (error) {
+      next(error)
+    }
+  })
+
+  // ? Lists all the neighborhood in zone
+  router.get('/:zone', async (req, res, next) => {
+    const { locationId } = req.params
+    try {
+      const location = await locationsServices.getOneLocation({ locationId })
+      res.status(200).json({
+        data: location,
+        message: 'Neighborhood listed'
       })
     } catch (error) {
       next(error)
