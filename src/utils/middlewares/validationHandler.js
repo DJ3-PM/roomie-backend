110fe35dff3dfa3989b95b87e6c7f90cd65b2fe6
Jose Cuevas <josecueram@gmail.com>
2020-07-02 00:29:26 -0500

    [ADD] Create User Schema Validation

---
+++ src/utils/middlewares/validationHandler.js
@@ -0,0 +1,17 @@
+const joi = require('@hapi/joi')
+const boom = require('@hapi/boom')
+
+const validate = (data, schema) => {
+  const { error } = joi.object(schema).validate(data)
+  return error
+}
+
+const validationHandler = (schema, check = 'body') => {
+  return (req, res, next) => {
+    const error = validate(req[check], schema)
+
+    error ? next(boom.badRequest(error)) : next()
+  }
+}
+
+module.exports = validationHandler
