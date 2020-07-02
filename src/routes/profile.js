0f6c564869eb0e66331ee295bd359cd5d2432028
Jose Cuevas <josecueram@gmail.com>
2020-07-02 00:59:04 -0500

    [ADD] Create Profile Schema Validation

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,9 @@
 const express = require('express')
 
 const imageUpload = require('../utils/middlewares/imageUpload')
+const validationHandler = require('../utils/middlewares/validationHandler')
+const { createProfileSchema } = require('../utils/schemas/profileValidation')
+
 const ProfileService = require('../services/profile')
 
 const profileRoutes = app => {
@@ -9,29 +12,32 @@ const profileRoutes = app => {
   app.use('/api/profile', router)
 
   // ? Creates a Profile
-  router.post('/', imageUpload.single('image'), async (req, res, next) => {
-    const profile = req.body
-    const { file } = req
+  router.post('/',
+    imageUpload.single('image'),
+    validationHandler(createProfileSchema),
+    async (req, res, next) => {
+      try {
+        const profile = req.body
+        const { file } = req
 
-    profile.avatar = file.location // obtain AWS S3 Image URL
+        profile.avatar = file.location // obtain AWS S3 Image URL
 
-    // 'parse' value
-    if (profile.isHost === 'true') {
-      profile.isHost = true
-    } else {
-      profile.isHost = false
-    }
+        // 'parse' value
+        if (profile.isHost === 'true') {
+          profile.isHost = true
+        } else {
+          profile.isHost = false
+        }
 
-    try {
-      const createdProfileId = await ProfileService.createProfile({ profile })
-      res.status(201).json({
-        data: createdProfileId,
-        message: 'Profile created!'
-      })
-    } catch (error) {
-      next(error)
-    }
-  })
+        const createdProfileId = await ProfileService.createProfile({ profile })
+        res.status(201).json({
+          data: createdProfileId,
+          message: 'Profile created!'
+        })
+      } catch (error) {
+        next(error)
+      }
+    })
 
   // TODO: Update/Modify Profile
   router.put('/:profileId', (req, res, next) => {
