4d1763908fb6f6c099d9c868557e5c7740f85e84
Jose Cuevas <josecueram@gmail.com>
2020-07-02 22:28:12 -0500

    [UPDATE] Removed TODO

--- src/services/users.js
+++ src/services/users.js
@@ -2,8 +2,6 @@ const bcrypt = require('bcrypt')
 const UserSchema = require('../utils/schemas/users')
 
 const createUser = async ({ user }) => {
-  // TODO: Validate unique username (email)
-
   const { username, password } = user
 
   const existingUser = await UserSchema.find({ username })
