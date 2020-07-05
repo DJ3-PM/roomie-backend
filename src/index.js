47941ea1caac8c1af5c4c89e2c71f51a0b6cc4fa
Jose Cuevas <josecueram@gmail.com>
2020-07-05 02:38:21 -0500

    [ADD] CORS Allowance for testing

--- src/index.js
+++ src/index.js
@@ -18,6 +18,15 @@ db.connect()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// ! REMOVE LATER WHEN READY FOR PRODUCTION
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+  next()
+})
+
 app.get('/', (req, res, next) => {
   res.send(':D')
 })
