c7ccdc3ef2df890b333553e4086d2236fc17ec09
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-05 18:04:28 -0500

    [ADD] setting variables in dotenv

--- config/index.js
+++ config/index.js
@@ -7,8 +7,13 @@ const config = {
   dbUser: process.env.DB_USER,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
   awsAccessKey: process.env.AWS_ACCESS_KEY,
-  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN
 }
 
 module.exports = config
