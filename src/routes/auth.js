ca67576c862b317429dd418554a49c8ef3b0200b
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-07 14:59:43 -0500

    [FIX] unused code

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const passport = require('passport')
 
 const usersService = require('../services/users')
 const validationHandler = require('../utils/middlewares/validationHandler')
